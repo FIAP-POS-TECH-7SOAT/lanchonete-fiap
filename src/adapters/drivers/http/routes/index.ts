00b6938ede8cf0fb0515224539b2cb40e5a5c20b
Thiago Rocha <tsrocha901@gmail.com>
2024-08-15 11:04:53 -0300

    add jwt validate rule

--- src/adapters/drivers/http/routes/index.ts
+++ src/adapters/drivers/http/routes/index.ts
@@ -5,8 +5,10 @@ import { productsRoutes } from "./product-routes";
 import { orderRoutes } from "./order-routes";
 import { orderStatusRoutes } from "./order-status-routes";
 import { clientRoutes } from "./client-routes";
+import { authMiddleware } from "../middlewares/auth-middleware";
 
 const routers = Router();
+routers.use(authMiddleware); // Todas rotas passarão pelo middleware para validar JWT
 
 routers.use("/payments", paymentsRoutes);
 routers.use("/products", productsRoutes);
