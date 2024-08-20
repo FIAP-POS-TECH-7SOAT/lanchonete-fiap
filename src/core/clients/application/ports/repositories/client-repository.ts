364bc5bbc50eeb81f20f0c971b0f59d819153424
Rafael Yuji Nakazono <rafa.yuji@gmail.com>
2024-08-19 21:46:12 -0300

    removendo arquivos relacionados a criação e alteração do domínio de clients

--- src/core/clients/application/ports/repositories/client-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "../../../domain/client-entity";
-import { CreateClientDTO } from "@application/clients/application/ports/repositories/dtos/client-dto";
-
-export interface IClientRepository {
-  findById(id: string): Promise<Client | null>;
-  findByEmail(email: string): Promise<Client | null>;
-  findByCpf(cpf: string): Promise<Client | null>;
-  create(data: CreateClientDTO): Promise<Client>;
-}
--- src/core/clients/application/use-case/Iclient-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "@application/clients/domain/client-entity";
-import { CreateClientDTO } from "@application/clients/application/ports/repositories/dtos/client-dto";
-
-export interface IClientService {
-  findById(id: string): Promise<Client | null>;
-  findByEmail(email: string): Promise<Client | null>;
-  findByCpf(cpf: string): Promise<Client | null>;
-  
-}
